49b2bdb37a6458755367fd140c9c6011535be3d4
MuratZhigitek <51789883+MuratZhigitek@users.noreply.github.com>
2019-09-21 20:13:19 -0400

    Update GitPractice.java
    made changes

--- src/test/java/GitPractice.java
+++ src/test/java/GitPractice.java
@@ -3,5 +3,7 @@ public class GitPractice {
     public static void main(String[] args) {
 
         System.out.println("Git Practice");
+        
+        System.out.println("Git Practice2 with Kuba Baike");
     }
 }
